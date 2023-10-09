5950ae6c3ebce887b5917d29cecbc0a14fb7a432
Justin <just.coder@outlook.com>
2023-10-09 20:52:30 +0100

    updates

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-console.log('I am Connected')
+// console.log('I am Connected')
 
 // Demo - 1
 /*
@@ -8,27 +8,71 @@ console.log('I am Connected')
 // Console.log & methods = alert, prompt & confirm (https://www.w3schools.com/jsref/obj_window.asp)
 // Devtools in chrome.
 
+// I am a comment
+
+console.log('Hi I am the console');
+alert('I am a alert');
+prompt('what is your name?');
+confirm('I you good today?');
 */
 
 // Demo - 2
 /*
 // Create the different types of variables
+// Show different ways of naming i.e. camel case, pascal case, snake case
 // Demostrate Hiosting & Block Scope
 
+// console.log(VarVariable, const_Variable);
+
+var VarVariable; // this pascal
+let letVariable; // this camel
+const const_Variable = 'Justin'; // this snake
+
+VarVariable = 21;
+letVariable = 'Heath'
+// const_Variable = 'Smith'
+
+// console.log(VarVariable, letVariable, const_Variable);
+
+const func = function () {
+    var VarVariable2 = 'var inside function'
+    let letVariable2 = 'let inside function'
+    console.log('block', VarVariable2, letVariable2);
+}
+
+func();
+
+console.log('global', VarVariable2, letVariable2);
 */
 
 // Demo - 3
 /*
 // Talk about primitives and objects https://developer.mozilla.org/en-US/docs/Glossary/Primitive
-// Show type of for the data types
+// Show 'type of' for the data types
 // Show how to do Arithmetic
 // Show Math method (https://www.w3schools.com/js/js_math.asp)
 
+const varTemp1 = Math.PI
+const varTemp2 = 3
+
+console.log(Math.floor(4.9));
 */
 
 // Demo - 4
 /*
 // Show how to concatenate strings
 // Show template literals
+// Show string cannot go over multiple lines
 // Show string methods i.e. length, toLowerCase, etc.. (https://www.w3schools.com/js/js_string_methods.asp)
+
+let someWord = 'justin'
+let someOtherWord = 'heath'
+// let someString = 'Justin 
+// Heath'
+let anotherWord = `Justin ${10 * 10} Heath`
+
+console.log("I'd like to use an apostrophe" + ' ' + someWord);
+console.log(someOtherWord + ' ' + someWord);
+console.log(anotherWord.toUpperCase());
 */
+
