1ada292486ffd95ca8738a9acbfde4d6d4588319
Justin <just.coder@outlook.com>
2023-10-09 15:50:28 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+console.log('I am Connected')
+
+// Demo - 1
+/*
+// Show different conenction methods
+// Demostrate how to use liveserver
+// How to comment in JS
+// Console.log & methods = alert, prompt & confirm (https://www.w3schools.com/jsref/obj_window.asp)
+// Devtools in chrome.
+
+*/
+
+// Demo - 2
+/*
+// Create the different types of variables
+// Demostrate Hiosting & Block Scope
+
+*/
+
+// Demo - 3
+/*
+// Talk about primitives and objects https://developer.mozilla.org/en-US/docs/Glossary/Primitive
+// Show type of for the data types
+// Show how to do Arithmetic
+// Show Math method (https://www.w3schools.com/js/js_math.asp)
+
+*/
+
+// Demo - 4
+/*
+// Show how to concatenate strings
+// Show template literals
+// Show string methods i.e. length, toLowerCase, etc.. (https://www.w3schools.com/js/js_string_methods.asp)
+*/
